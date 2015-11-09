67ca4e863b27d7852423b0512f57f67a2f251e1e
Sally McNichols <smcnicho@andrew.cmu.edu>
2015-11-09 17:28:20 -0500

    Updated UI

---
+++ Calc Training/Calc Training/PlayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PlayViewController.swift
+//  Calc Training
+//
+//  Created by Sally McNichols on 11/9/15.
+//  Copyright © 2015 Andy Liu. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    @IBOutlet var numberInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
